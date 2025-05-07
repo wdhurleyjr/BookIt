e618f9ea6f94af06a04fd148533008384b165f88
aleguzmancs9 <aleguzmancs9@gmail.com>
2025-05-06 23:46:33 -0700

    Initial commit for Heroku deployment

--- index.mjs
+++ index.mjs
@@ -205,6 +205,7 @@ app.get('/api/getPins', isAuthenticated, async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Express server running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Express server running on port ${PORT}`);
 });
